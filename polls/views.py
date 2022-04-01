76163e6bbed2a6a895ce32dc83868c844f666796
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-01 17:18:33 -0400

    set root directory - test

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib import admin
 
 def index(request):
-    if request.user.is_authenticated:
-        return HttpResponse("""<!DOCTYPE html>
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (admin.site.urls, request.path))
+    return HttpResponse("""<!DOCTYPE html>
                             <html lang="en">
                             <head>
                             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+                            
                             </head>
                             <meta charset="UTF-8">
                             <title>Page Title</title>
@@ -17,10 +18,10 @@ def index(request):
                             <style>
                             </style>
                             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+                            
                             <script src=""></script>
                             <body>
-
+                            
                             <div class="">
                              <h1>This is a Heading</h1>
                              <p>This is a paragraph.</p>
@@ -42,13 +43,9 @@ def index(request):
                                 </div>
                               </div>
                             </div>
-
+                            
                             </body>
                             </html>
                             """)
 
-        # Do something for authenticated users.
-        ...
-    else:
-        return HttpResponse("UNAUTHENTICATED")
 # Create your views here.
