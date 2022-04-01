03889821f39a2215db0a0f87225980afe2feaeff
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-01 15:34:17 -0400

    test pipelines

--- polls/views.py
+++ polls/views.py
@@ -28,7 +28,7 @@ def index(request):
                             <div class="container">
                               <div class="row">
                                 <div class="col-sm-4">
-                                  <h3>Column 1</h3>
+                                  <h3>Column 1t</h3>
                                   <p>Lorem ipsum dolor..</p>
                                 </div>
                                 <div class="col-sm-4  bg-primary text-white">
