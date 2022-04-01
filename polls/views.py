31e3e97a227d619d9e83f39fae71ec9b99f5c848
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-01 18:52:24 -0400

    updated views.py

--- polls/views.py
+++ polls/views.py
@@ -3,27 +3,28 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib import admin
 from django.template import loader
 from django.urls import reverse
-
+from django.views import generic
 
 from polls.models import Question, Choice
 
 
-def index(request):
-    # if not request.user.is_authenticated:
-    #     return redirect(admin.site.urls)
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
