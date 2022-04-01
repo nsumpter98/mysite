7ff2e37f7c4fa6430aba3bf25342404c7c01f8e2
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-01 19:44:29 -0400

    edit admin side

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,11 @@
 from datetime import timezone, datetime
 
-from django.contrib import admin
+from django.contrib.auth import admin
 
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
@@ -17,19 +17,8 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
 
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
 
-    @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
-    )
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
+admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Question)
