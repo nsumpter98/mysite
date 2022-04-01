aa717361ec7998d81f950951ac057cb200545fdc
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-01 17:38:53 -0400

    templates/views

--- polls/views.py
+++ polls/views.py
@@ -1,52 +1,27 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.contrib import admin
+from django.template import loader
+
+from polls.models import Question
 
 
 def index(request):
     #if not request.user.is_authenticated:
    #     return redirect(admin.site.urls)
-    return HttpResponse("""<!DOCTYPE html>
-                            <html lang="en">
-                            <head>
-                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-                            
-                            </head>
-                            <meta charset="UTF-8">
-                            <title>Page Title</title>
-                            <meta name="viewport" content="width=device-width,initial-scale=1">
-                            <link rel="stylesheet" href="">
-                            <style>
-                            </style>
-                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-                            
-                            <script src=""></script>
-                            <body>
-                            
-                            <div class="">
-                             <h1>This is a Heading</h1>
-                             <p>This is a paragraph.</p>
-                             <p>This is another paragraph.</p>
-                            </div>
-                            <div class="container">
-                              <div class="row">
-                                <div class="col-sm-4">
-                                  <h3>Test</h3>
-                                  <p>Lorem ipsum dolor..</p>
-                                </div>
-                                <div class="col-sm-4  bg-primary text-white">
-                                  <h3>Column 2</h3>
-                                  <p>Lorem ipsum dolor..</p>
-                                </div>
-                                <div class="col-sm-4">
-                                  <h3>Column 3</h3>
-                                  <p>Lorem ipsum dolor..</p>
-                                </div>
-                              </div>
-                            </div>
-                            
-                            </body>
-                            </html>
-                            """)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    def detail(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        return render(request, 'polls/detail.html', {'question': question})
+
+    def results(request, question_id):
+        response = "You're looking at the results of question %s."
+        return HttpResponse(response % question_id)
+
+    def vote(request, question_id):
+        return HttpResponse("You're voting on question %s." % question_id)
 
 # Create your views here.
