3df2c689b02cb42b7ff1dd846ac5fd1a70d7f333
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-01 19:44:42 -0400

    edit admin side

--- polls/admin.py
+++ polls/admin.py
@@ -20,6 +20,5 @@ class QuestionAdmin(admin.ModelAdmin):
 
 admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Question)
 # admin.site.register(Choice)
