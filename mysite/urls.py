8ab9ecf74b22d80f96dff200f8ff76cf299b9a2e
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-05 13:28:50 -0400

    added stencil app

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('dashboard/', TemplateView.as_view(template_name='dashboard/index.html')),
+    path('', TemplateView.as_view(template_name='data_visual/index.html'))
 ]
