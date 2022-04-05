91c991ef6effa27c0ad3cda0af70b8290f9de34a
nsump <nsump@DESKTOP-RV8UIU1>
2022-04-05 14:00:05 -0400

    added stencil app

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = ''
+STATIC_URL = '/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'data_visual/images'),
